42df5bdc5ede4441ace88b5a038e2306125a90a8
Johnny Crash <cannon.imus@gmail.com>
2017-11-06 13:05:09 +0100

    Update CI parts, take 3

--- README.md
+++ README.md
@@ -4,10 +4,8 @@ Host various OpenGL examples.
 # Create a new dispute (a.k.a. project)
 ```
 opengl_disputes % DISPUTE_NAME=test_dispute make
-opengl_disputes % cd test_dispute
-the_box % mkdir build
-the_box % cd build
-build % cmake ..
-build % make
-build % ./the_box
+opengl_disputes % mkdir -p test_dispute/build
+opengl_disputes % cd test_dispute/build && cmake .. && cd -
+opengl_disputes % cd test_dispute/build && make && cd -
+opengl_disputes % ./test_dispute/build/test_dispute
 ```
--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 
@@ -6,11 +7,22 @@ THIS_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
 
 def main():
+    commands_in_readme_md = extract_commands_from_readme_md()
+    execute_commands(commands_in_readme_md)
+
+
+def extract_commands_from_readme_md():
     with open(os.path.join(THIS_DIRECTORY, "README.md")) as readme:
-        commands_to_execute = [
-            line for line in readme if " % " in line
+        return [
+            line[line.index("%") + 2:].rstrip()
+            for line in readme
+            if " % " in line
         ]
-    print(commands_to_execute)
+
+
+def execute_commands(commands_in_readme_md):
+    for command in commands_in_readme_md:
+        subprocess.Popen(command, shell=True).communicate()
 
 
 if __name__ == "__main__":
