0d0db9279df84fef69fa52ed81351bf764a55c7c
Johnny Crash <cannon.imus@gmail.com>
2017-11-07 12:17:03 +0100

    Add --version command line option handling

--- cookiecutter_template/{{cookiecutter.opengl_dispute_name}}/{{cookiecutter.opengl_dispute_name}}.cpp
+++ cookiecutter_template/{{cookiecutter.opengl_dispute_name}}/{{cookiecutter.opengl_dispute_name}}.cpp
@@ -6,8 +6,36 @@
 #include "GLFW/glfw3.h"
 
 
-int main() {
+int parse_program_options(int argc, char* argv[]);
+
+
+int parse_program_options(int argc, char* argv[]) {
+  boost::program_options::options_description generic_options("Generic options");
+
+  generic_options.add_options()
+    ("version,v", "print version string")
+  ;
+
+  boost::program_options::options_description command_line_options;
+  command_line_options.add(generic_options);
+
+  boost::program_options::variables_map variables_map;
+  store(boost::program_options::command_line_parser(argc, argv).
+    options(command_line_options).run(), variables_map);
+  notify(variables_map);
+
+  if (variables_map.count("version")) {
+    std::cout << "Version 1.0" << std::endl;
+  }
+
+  return 0;
+}
+
+
+int main(int argc, char* argv[]) {
   std::cout << "OpenGL Dispute: {{cookiecutter.opengl_dispute_name}}" << std::endl;
+
+  parse_program_options(argc, argv);
 
   return 0;
 }
